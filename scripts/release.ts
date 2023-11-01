94cb488349da54c637e8afa17982b98ebd60d904
MuRong <372638156@qq.com>
2023-11-01 10:49:11 +0800

    chore: release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -26,14 +26,14 @@ async function release() {
 
   await updateTemplateVersion()
 
-  // execSync('npm run build', { stdio: 'inherit' })
-  // execSync('git add .', { stdio: 'inherit' })
+  execSync('npm run build', { stdio: 'inherit' })
+  execSync('git add .', { stdio: 'inherit' })
 
-  // execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
-  // execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
+  execSync(`git commit -m "chore: release v${version}"`, { stdio: 'inherit' })
+  execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
 
-  // execSync('git push', { stdio: 'inherit' })
-  // execSync('git push --tags', { stdio: 'inherit' })
+  execSync('git push', { stdio: 'inherit' })
+  execSync('git push --tags', { stdio: 'inherit' })
 }
 
 async function updateTemplateVersion() {
