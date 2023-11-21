5556de9f1b1c1bc25a71fd6be575e83e8b832533
MuRong <372638156@qq.com>
2023-11-21 09:22:33 +0800

    test: clean code

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,37 +2,6 @@ import { describe, expect, it } from 'vitest'
 import { checkPinataAuthentication, fromHexString /* isEthereumAddress, loadZKGraphSources, parseYaml */ } from '../packages/zkgraph-cli/src/utils'
 
 describe('utils', () => {
-  // it('test loadYamlContent', async () => {
-  //   const yamlPath = path.join(__dirname, 'fixtures/utils/test.yaml')
-  //   const yamlContent = fs.readFileSync(yamlPath, 'utf-8')
-  //   const parser = await parseYaml(yamlContent)
-  //   expect(parser.repository).toBe('https://github.com/hyperoracle/zkgraph')
-  //   expect(parser.dataSources[0].source.address).toBe('0xa60ecf32309539dd84f27a9563754dca818b815e')
-  //   expect(parser).toMatchSnapshot()
-  // })
-
-  // it('test isEthereumAddress', () => {
-  //   expect(isEthereumAddress('0xa60ecf32309539dd84f27a9563754dca818b815e')).toBe(true)
-  //   expect(isEthereumAddress('0x0000000000000000000000000000000000000001')).toBe(true)
-  //   expect(isEthereumAddress('0x00000000000000000000000000000000000001')).toBe(false)
-  //   expect(isEthereumAddress('0x0000000000000000000000000000000000000000')).toBe(false)
-  // })
-
-  // it('test yamlHealthCheck', async () => {
-  //   const yamlPath = path.join(__dirname, 'fixtures/utils/test-yaml-check.yaml')
-  //   const yamlContent = fs.readFileSync(yamlPath, 'utf-8')
-  //   const parser = await parseYaml(yamlContent)
-  //   expect(() => yamlHealthCheck(parser)).not.toThrow()
-  // })
-
-  // it('test loadZKGraphSources', async () => {
-  //   const yamlPath = path.join(__dirname, 'fixtures/utils/test.yaml')
-  //   const yamlContent = fs.readFileSync(yamlPath, 'utf-8')
-  //   const parser = await parseYaml<ZkGraphYaml>(yamlContent)
-  //   const [source_address] = loadZKGraphSources(parser)
-  //   expect(source_address).toEqual(parser.dataSources.map(item => item.source.address))
-  // })
-
   it('test fromHexString', () => {
     expect(fromHexString('0x0000000000000000000000000000000000000000')).toEqual(new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
   })
